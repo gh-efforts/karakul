2dacd9494716b6d27c6cf8768ebdc08816cae477
Eva <690090823@qq.com>
2020-08-24 16:56:40 +0800

    feat: 添加header种的用户部分以及优化svg

--- src/layout/sider/index.tsx
+++ src/layout/sider/index.tsx
@@ -1,14 +1,48 @@
 import { Menu } from "antd";
 import styles from "./index.module.scss";
 import Svg from "../../components/Svg";
-import { useState } from "react";
+import React, { useState } from "react";
 const { SubMenu } = Menu;
 function KSider() {
-  const [selectedKeys, setSelectedKeys] = useState<string[]>([]);
+  const [orderItemSelected, setOrderItemSelected] = useState(false);
+  const [goodsItemSelected, setGoodsItemSelected] = useState(false);
   const onSelect = (values: any) => {
-    console.log(values);
-    setSelectedKeys(values.selectedKeys);
+    if (values.selectedKeys.indexOf("1") > -1) {
+      setOrderItemSelected(true);
+      setGoodsItemSelected(false);
+    } else if (
+      values.selectedKeys.indexOf("4") > -1 ||
+      values.selectedKeys.indexOf("5") > -1 ||
+      values.selectedKeys.indexOf("6") > -1
+    ) {
+      setOrderItemSelected(false);
+      setGoodsItemSelected(true);
+      console.log(goodsItemSelected);
+    } else {
+      setOrderItemSelected(false);
+      setGoodsItemSelected(false);
+    }
   };
+
+  const OrderSvg = (color: string) => (
+    <Svg
+      name="ico-order-s"
+      width="24px"
+      height="24px"
+      color={color}
+      offsetY="6"
+    />
+  );
+
+  const GoodsSvg = (color: string) => (
+    <Svg
+      name="ico-goods-n"
+      width="24px"
+      height="24px"
+      color={color}
+      offsetY="6"
+    />
+  );
   return (
     <div>
       <div className={styles.logo} />
@@ -17,41 +51,15 @@ function KSider() {
         mode="inline"
         theme="light"
         defaultSelectedKeys={["1"]}
-        selectedKeys={selectedKeys}
         onSelect={onSelect}
       >
-        <Menu.Item
-          key="1"
-          icon={
-            selectedKeys.indexOf("1") > -1 ? (
-              <Svg name="ico-order-s" width="24px" height="24px" color="#fff" />
-            ) : (
-              <Svg name="ico-order-s" width="24px" height="24px" color="#222" />
-            )
-          }
-        >
+        <Menu.Item key="1" icon={OrderSvg(orderItemSelected ? "#fff" : "#222")}>
           订单管理
         </Menu.Item>
         <SubMenu
           key="sub2"
           title="商品管理"
-          icon={
-            selectedKeys.indexOf("1") > -1 ? (
-              <Svg
-                name="ico-goods-n"
-                width="24px"
-                height="24px"
-                color="#222"
-              ></Svg>
-            ) : (
-              <Svg
-                name="ico-goods-n"
-                width="24px"
-                height="24px"
-                color="#00b2b6"
-              />
-            )
-          }
+          icon={GoodsSvg(goodsItemSelected ? "#00b2b6" : "#222")}
         >
           <Menu.Item key="4">商品列表</Menu.Item>
           <Menu.Item key="5">商品流向</Menu.Item>
