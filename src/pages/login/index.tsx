bf49d984b8ba458afb31e55f3434ff5851e541c8
Eva <690090823@qq.com>
2020-08-26 17:52:30 +0800

    fix: 登录流程完成

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -1,11 +1,37 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Link from "next/link";
 import getConfig from "next/config";
+import { useRouter } from "next/router";
+import { isArray, isObject } from "util";
 export default function Login() {
   const {
     publicRuntimeConfig: { ENDPOINT },
   } = getConfig();
   const backendUrl = ENDPOINT;
+  const router = useRouter();
+  useEffect(() => {
+    console.log(router);
+    const callback = router.asPath.split("?");
+    fetch(`${backendUrl}/auth/feishu/callback?${callback[1]}`)
+      .then((res) => {
+        if (res.status !== 200) {
+          throw new Error(`Couldn't login to Strapi. Status: ${res.status}`);
+        }
+        return res;
+      })
+      .then((res) => res.json())
+      .then((res) => {
+        // Successfully logged with Strapi
+        // Now saving the jwt to use it for future authenticated requests to Strapi
+        localStorage.setItem("jwt", res.jwt);
+        localStorage.setItem("user", res.user);
+
+        setTimeout(() => router.push("/"), 3000); // Redirect to homepage after 3 sec
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, [router]);
   return (
     <div>
       <Link href={`${backendUrl}/connect/feishu`}>
