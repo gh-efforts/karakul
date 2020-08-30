c026465fed3ddbbd7db41a4640d2700c7012c7e2
Eva <690090823@qq.com>
2020-08-30 23:43:07 +0800

    feat: 添加原材料管理

---
+++ src/pages/material/layout/modal/creat-modal.tsx
@@ -0,0 +1,28 @@
+import React from 'react'
+
+import CreateForm from '../form/creat-form'
+import { KTable, useGlobalModal } from '../../../../components'
+import { OrderMaterial } from '../../../../services'
+import modalColumns from '../table/create-modal-column'
+import ModalView from './modal'
+function CreateModalView(): React.ReactElement {
+  const { hideModal } = useGlobalModal()
+
+  const onOK = () => {
+    hideModal()
+  }
+  return (
+    <ModalView orderId={'123'} OKText='创建' onOK={onOK}>
+      <CreateForm />
+      <KTable<OrderMaterial>
+        data={[] as OrderMaterial[]}
+        columns={modalColumns}
+        total={10}
+        currentPage={1}
+        rowKey={item => item.id}
+        pagination={false}
+      />
+    </ModalView>
+  )
+}
+export default CreateModalView
---
+++ src/pages/material/layout/table/column.tsx
@@ -0,0 +1,31 @@
+import { OrderMaterial } from '../../../../services'
+import { ColumnProps } from 'antd/lib/table'
+
+const columns: ColumnProps<OrderMaterial>[] = [
+  {
+    title: '分类',
+    dataIndex: 'name',
+  },
+  {
+    title: '型号',
+    dataIndex: 'callType',
+  },
+  {
+    title: '数量',
+    dataIndex: 'type',
+  },
+  {
+    title: '创建时间',
+    dataIndex: 'createdAt',
+  },
+  {
+    title: '更新时间',
+    dataIndex: 'updatedAt',
+  },
+  {
+    title: '操作',
+    width: 220,
+  },
+]
+
+export default columns
