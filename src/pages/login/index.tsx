a5b440052622eb8d8da6ed54711a2bb2a7f0d832
Vache <mervinjin@gmail.com>
2020-09-04 11:51:44 +0800

    chore: login page style (#37)

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -2,6 +2,7 @@ import React, { useEffect } from 'react'
 import Link from 'next/link'
 import getConfig from 'next/config'
 import { useRouter } from 'next/router'
+import styles from './index.module.scss'
 
 import { setLocalCookie, setLocalStorage } from '../../helpers/cookie'
 
@@ -36,10 +37,24 @@ export default function Login(): React.ReactElement {
   }, [router, backendUrl])
 
   return (
-    <div>
-      <Link href={`${backendUrl}/connect/feishu`}>
-        <span>飞书登录</span>
-      </Link>
+    <div className={styles.container}>
+      <div className={styles.header}>
+        <img className={styles.logo} src='/images/logo.svg' alt='logo' />
+      </div>
+      <div className={styles.content}>
+        <div className={styles['action-wrapper']}>
+          <Link href={`${backendUrl}/connect/feishu`}>
+            <button className={styles.action}>飞书登录</button>
+          </Link>
+        </div>
+        <div className={styles.welcome}>
+          <img className={styles.logo} src='/images/logo-inverse.svg' alt='logo' />
+          <div style={{ margin: 'auto 0' }}>
+            <h1 className={styles.slogan}> Welcome to Collie</h1>
+            <p className={styles.tip}>Login with Feishu</p>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
