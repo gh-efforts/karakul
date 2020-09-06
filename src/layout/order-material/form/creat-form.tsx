503ce80a68d37cd62fd6ed18658f87ff16c4cb40
Eva <690090823@qq.com>
2020-09-06 14:58:14 +0800

    fix: 优化表格操作以及一些bug

--- src/layout/order-material/form/creat-form.tsx
+++ src/layout/order-material/form/creat-form.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import { Form, Input, Button } from 'antd'
 
 import styles from './index.module.scss'
-import { MaterialsSelect } from 'src/components'
+import { MaterialsSelect, getRealValue } from 'src/components'
 import { Store } from 'antd/lib/form/interface'
 import { Material } from '../material'
 
@@ -15,7 +15,12 @@ export default function CreateForm({ onSubmit }: CreateFormProps) {
   const onFinish = (values: Store) => {
     const { amount, material, model } = values
     if (amount && material && model) {
-      onSubmit({ id: Math.random(), amount, material, model, action: 4 })
+      const [mid, mname] = getRealValue(material)
+      const KMaterial = {
+        id: mid,
+        name: mname,
+      }
+      onSubmit({ id: Math.random(), amount, material: KMaterial, model, action: 4 })
       form.resetFields()
     }
   }
@@ -27,12 +32,12 @@ export default function CreateForm({ onSubmit }: CreateFormProps) {
   return (
     <div className={styles['create-form']}>
       <Form layout={'inline'} form={form} onFinish={onFinish} onReset={onReset}>
-        <MaterialsSelect name='material' noLabel noSplit style={{ width: 188 }} required />
+        <MaterialsSelect name='material' noLabel style={{ width: 188 }} required />
         <Form.Item name='model'>
-          <Input size='large' placeholder='请输入型号' />
+          <Input size='large' placeholder='请输入型号' allowClear />
         </Form.Item>
         <Form.Item name='amount' rules={[{ required: true, message: '请输入数量' }]}>
-          <Input size='large' type='number' placeholder='请输入数量' />
+          <Input size='large' type='number' placeholder='请输入数量' allowClear />
         </Form.Item>
         <Form.Item>
           <Button type='link' htmlType='submit'>
